2a95ad1593b71e540230b1218b6c0dfc2dea56ba
Alexey Ershov <alex4rom@gmail.com>
2020-07-12 17:20:31 +0300

    Refactor

--- x11.cpp
+++ x11.cpp
@@ -2,7 +2,7 @@
 // cc x11.c -I /usr/local/include -L /usr/local/lib/ -l X11
 
 // Linux
-// cc x11.cpp -I /usr/local/include -L /usr/lib/x86_64-linux-gnu/X11 -l X11 -lm -lstdc++
+// cc x11.cpp -I /usr/local/include -L /usr/lib/x86_64-linux-gnu/X11 -l X11 -lm -lstdc++// In freebsd
 
 #include <X11/Xlib.h>
 #include <stdio.h>
@@ -46,43 +46,23 @@ vec3 color(const ray& r) {
   return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
-// end Ray helper functions
+int w_height = 100;
+int w_width = 200;
 
-int main(void) {
-  // X window setup
-  Display *disp;
-  Window win;
-  XEvent event;
-  const char *msg = "Hello, World!";
-  int screen;
+// Ray setup
+vec3 lower_left_corner(-2.0, -1.0, -1.0);
+vec3 horizontal(4.0, 0.0, 0.0);
+vec3 vertical(0.0, 2.0, 0.0);
+vec3 origin = vec3(0.0, 0.0, 0.0);
+// end Ray setup
 
-  int w_height = 100;
-  int w_width = 200;
+// Xlib variables
+Display *disp;
+Window win;
+int screen;
+// End Xlib variables
 
-  disp = XOpenDisplay(NULL);
-  if (disp == NULL) {
-     fprintf(stderr, "Cannot open display\n");
-     exit(1);
-  }
-
-  screen = DefaultScreen(disp);
-  win = XCreateSimpleWindow(disp, RootWindow(disp, screen), 0, 0, w_width, w_height, 0,
-                           BlackPixel(disp, screen), WhitePixel(disp, screen));
-  XSelectInput(disp, win, ExposureMask | KeyPressMask);
-  XMapWindow(disp, win);
-  // end X window setup
-
-  // Ray setup
-  vec3 lower_left_corner(-2.0, -1.0, -1.0);
-  vec3 horizontal(4.0, 0.0, 0.0);
-  vec3 vertical(0.0, 2.0, 0.0);
-  vec3 origin(0.0, 0.0, 0.0);
-  // end Ray setup
-  
-  while (1) {
-     XNextEvent(disp, &event);
-     if (event.type == Expose) {
-
+void draw() {
        for (int j = w_height - 1; j >= 0; j--) {
 	 for (int i = 0; i < w_width; i++) {
 	   float u = float(i) / float(w_width - 1);
@@ -101,18 +81,49 @@ int main(void) {
 	   XDrawPoint(disp, win, DefaultGC(disp, screen), i, j);
 	  }
        }
+}
+
+// end Ray helper functions
+
+int main(void) {
+  // X window setup
+  XEvent event;
+  const char *msg = "Hello, World!";
+
+  disp = XOpenDisplay(NULL);
+  if (disp == NULL) {
+     fprintf(stderr, "Cannot open display\n");
+     exit(1);
+  }
+
+  screen = DefaultScreen(disp);
+  win = XCreateSimpleWindow(disp, RootWindow(disp, screen), 0, 0, w_width, w_height, 0,
+                           BlackPixel(disp, screen), WhitePixel(disp, screen));
+  XSelectInput(disp, win, ExposureMask | KeyPressMask);
+  XMapWindow(disp, win);
+  // end X window setup
+
+
+  
+  while (1) {
+     XNextEvent(disp, &event);
+     if (event.type == Expose) {
+       draw();
      }
      if (event.type == KeyPress) {
+       // We'll move camera depending on a key pressed
        if (event.xkey.keycode == W_KEY) {
-	 XSetForeground(disp, DefaultGC(disp, screen), _rgb(0, 0, 0));
-	 XFillRectangle(disp, win, DefaultGC(disp, screen), 30, 30, 10, 10);
+	 // Change the view and draw everythin again
+	 // NOTE: just change the origin for now
+	 // TODO: make the actuall correct camera rotation
+	 origin = vec3(1.0, 0.0, 0.0);
+	 
+	 draw();
        }
 
        if (event.xkey.keycode == Q_KEY) {
 	 break;
        }
-       //XSetForeground(disp, DefaultGC(disp, screen), _rgb(255, 0, 127));
-       //XFillRectangle(disp, win, DefaultGC(disp, screen), 30, 30, 10, 10);
      }
   }
  
