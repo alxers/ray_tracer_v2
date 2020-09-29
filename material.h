665770ae245bad484b97a60a749f83c12503b232
Alexey Ershov <alex4rom@gmail.com>
2020-09-29 22:54:32 +0300

    Comment

--- material.h
+++ material.h
@@ -12,7 +12,9 @@ struct material {
 vec3 random_in_unit_sphere() {
   vec3 p;
   do {
+    // Pick a point in the unit cube
     p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    // If it's not on a unit sphere reject it and try again
   } while (dot(&p, &p) >= 1);
   return p;
 }
